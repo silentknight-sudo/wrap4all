305bbde45b0dac04c22812bc14c7cc148275a44f
Vivek Shukla <playsidgaming@gmail.com>
2026-03-03 11:26:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1687185635-cd116",
+  "appId": "1:128142419030:web:c5a808c8a45c0a149947f7",
+  "apiKey": "AIzaSyAJOQer4z6CjwLoEI5XoG3gNkHoRVsA2eM",
+  "authDomain": "studio-1687185635-cd116.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "128142419030"
+};
